511be61874afa4c9ffaa4d8ec2df87e34c7af832
juan <jcorujo@correo.um.edu.uy>
2023-11-02 02:00:54 -0300

    gestores

--- gestor_equipos.py
+++ gestor_equipos.py
@@ -1,3 +1,24 @@
-class Pista:
+class Gestor_Equipos:
     def __init__(self, equipos):
         self._equipos = []
+    #CREAR EXCEPCIONES
+    @property
+    def equipos(self):
+        return self._equipos
+    
+    def buscar_equipo(self, nombre_equipo):
+        equipo_encontrado = None
+        for equipo in self._equipos:
+            if equipo.nombre == nombre_equipo:
+                equipo_encontrado = equipo
+        return equipo_encontrado
+    
+    def agregar_equipo(self, equipo):
+        #Verificar que equipo no exista
+        pass
+    
+    def cambiar_nombre(self, equipo, nuevo_nombre):
+        pass
+   
+    def traspasar_pilotos(self, equipo, piloto):
+        pass
