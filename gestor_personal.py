8dc6efa6b5b8526cc909fd5323e922ac2b1720fc
juan <jcorujo@correo.um.edu.uy>
2023-11-02 21:01:59 -0300

    2/11/23

---
+++ gestor_personal.py
@@ -0,0 +1,14 @@
+ def agregar_piloto(self, equipo, nombre_piloto):
+              #VERIFICAR EXISTENCIA EQUIPO
+              #VERIFICAR CAPACIDAD EQUIPO
+              #CREAR PILOTO
+              #VERIFICAR QUE NO EXISTA PILOTO CON MISMO ID, EN EL EQUIPO U OTROS EQUIPOS
+              pass
+
+       def cambiar_nacionalidad(self, nombre_piloto, nacionalidad):
+              pass
+       #CAMBIAR SALARIO, AUTO, NOMBRE
+       def dar_baja(self, nombre):
+              pass
+       def dar_alta(self, nombre):
+              pass
